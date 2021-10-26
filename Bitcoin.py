533b208a1c2184a58ea0c04a3590246c8819aa61
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-10-26 12:10:56 +0200

    Update Bitcoin.py

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,1 +1,7 @@
-print(0Hello)
+def bitcoin(a, b):
+    euros = a * b
+    return(print("You have" +  euros))
+    if euros < 30000:
+        print("¡¡¡Tu inversión está por debajo de 30000€!!!")
+    else:
+        print("Tu inversión está por encima de 30000€")
