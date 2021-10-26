e5702c51fa31c92fa59bf4ceeb6a0aeecbcf0ab0
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-10-26 12:16:18 +0200

    Update Bitcoin.py

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,7 +1,9 @@
 def bitcoin(a, b):
-    euros = a * b
-    return(print("You have" +  euros))
+    euros = float(a) * float(b)
+    print("You have " +  str(euros))
     if euros < 30000:
         print("¡¡¡Tu inversión está por debajo de 30000€!!!")
     else:
-        print("Tu inversión está por encima de 30000€")
+        print("Tu inversión está por encima de 30000")
+
+bitcoin(1.2, 40000)
