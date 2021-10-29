009c1ed00bb75d7f8fd63045eb975dd460b91a38
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-10-29 09:08:39 +0200

    Update Shipping.py

--- Shipping.py
+++ Shipping.py
@@ -1,6 +1,6 @@
 coste_por_kilo = 1.20
 def compra(valor_cesta, peso_cesta):
-    if float(valor_cesta) > 100:
+    if float(valor_cesta) >= 100:
         print("Tu envío es gratuito")
     else:
         costetotal = float(peso_cesta) * coste_por_kilo
