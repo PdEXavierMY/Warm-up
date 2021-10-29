b2e46e3b40eeb51a81b82eb96ebfc0794d59a8ac
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-10-29 09:26:07 +0200

    Update Script.py

--- Script.py
+++ Script.py
@@ -5,3 +5,10 @@ print("Roses are #ff0000 Violets are #0000ff why my code´s working I haven´t a
 print("Hello world")
 
 #Tarea 3#
+print(21 + 43)
+print(142 - 52)
+print(10 * 342)
+print(5 ** 2)
+#La sintaxis realizada en los cuatro casos es de tipo integrer, es decir, un número entero#
+
+#Tarea 4#
