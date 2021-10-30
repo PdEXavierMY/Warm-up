05a0b38fc5520709cd763d51cbbcbb7354b654f0
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-10-30 18:07:42 +0200

    Update Script9.py

--- Script9.py
+++ Script9.py
@@ -1,1 +1,9 @@
 #Tarea 9#
+flag = open('flag.txt','r')
+print(flag.read())
+flag.close()
+flag_modified = open('flag.txt','a')
+flag_modified.write("Esta línea es nueva!!!")
+flag_modified = open('flag.txt','r')
+print(flag_modified.read())
+flag_modified.close()
